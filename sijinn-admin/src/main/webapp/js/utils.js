9f52c4595934accf333886d462c285940e5ebf44
svyatoslav urbanovych <svyatoslav.urbanovych@gmail.com>
2016-05-09 17:22:14 +0200

    work in progress

--- sijinn-admin/src/main/webapp/js/utils.js
+++ sijinn-admin/src/main/webapp/js/utils.js
@@ -1,7 +1,7 @@
 function createLineElement(id, x, y, length, angle) {
     var line = document.createElement("div");
     line.setAttribute('id',id);
-    var styles = 'border: 1px ridge #87C5F0; '
+    var styles = 'border_: 1px ridge #87C5F0; '
                + 'width: ' + length + 'px; '
                + 'height: 0px; '
                + '-moz-transform: rotate(' + angle + 'rad); '
@@ -10,14 +10,17 @@ function createLineElement(id, x, y, length, angle) {
                + '-ms-transform: rotate(' + angle + 'rad); '  
                + 'position: absolute; '
                + 'top: ' + y + 'px; '
-               + 'left: ' + x + 'px; '
+               + 'left: ' + x + 'px; ';
+/*    
     		   + 'opacity: 0.5; '
     		   + 'filter: alpha(opacity=50); '
     		   + '-moz-box-shadow: 1px 1px 2px gray; '
     		   + '-webkit-box-shadow: 1px 1px 2px gray; '
     		   + 'box-shadow: 1px 1px 2px gray; ';
+*/
+    line.setAttribute('style', styles); 
+    line.classList.add('synapse-line');
 
-    line.setAttribute('style', styles);  
     return line;
 }
 
@@ -36,4 +39,26 @@ function createLine(id, x1, y1, x2, y2) {
 
     return createLineElement(id, x, y, c, alpha);
 }
+
+function hasClass(el, className) {
+	  if (el.classList)
+	    return el.classList.contains(className)
+	  else
+	    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+	}
+
+	function addClass(el, className) {
+	  if (el.classList)
+	    el.classList.add(className)
+	  else if (!hasClass(el, className)) el.className += " " + className
+	}
+
+	function removeClass(el, className) {
+	  if (el.classList)
+	    el.classList.remove(className)
+	  else if (hasClass(el, className)) {
+	    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+	    el.className=el.className.replace(reg, ' ')
+	  }
+	}
 
